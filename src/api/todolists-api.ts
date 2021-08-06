49edfdbc4eee00c4ad2414dca0ded05ba0424495
JamesWhite10 <max.migalin10@gmail.com>
2021-08-06 21:12:51 +0300

    Add gh-pages.

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -4,7 +4,7 @@ const instance = axios.create({
     baseURL: 'https://social-network.samuraijs.com/api/1.1/',
     withCredentials: true,
     headers: {
-        'API-KEY': '1f8bceb1-8e01-4532-aa69-076eda261e41'
+        'API-KEY': '14b0628e-63da-450d-a3cb-ba811b8ac388'
     }
 })
 
